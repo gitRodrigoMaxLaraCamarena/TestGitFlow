37b0fe20e19d608311aba401a023a90b2939f1b0
Alvaro Poma <alvaro_pomas@hotmail.com>
2019-05-16 21:44:26 -0500

    activities: Main and EditAccount

---
+++ Nearme/app/src/main/java/resasoftware/com/pe/nearme/controllers/activities/EditAccountActivity.kt
@@ -0,0 +1,13 @@
+package resasoftware.com.pe.nearme.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import resasoftware.com.pe.nearme.R
+
+class EditAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_account)
+    }
+}
--- Nearme/app/src/main/java/resasoftware/com/pe/nearme/controllers/fragments/AccountFragment.kt
+++ Nearme/app/src/main/java/resasoftware/com/pe/nearme/controllers/fragments/AccountFragment.kt
@@ -1,13 +1,17 @@
 package resasoftware.com.pe.nearme.controllers.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import kotlinx.android.synthetic.main.fragment_account.*
 import resasoftware.com.pe.nearme.R
+import resasoftware.com.pe.nearme.controllers.activities.EditAccountActivity
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +23,7 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class AccountFragment : Fragment() {
+    var image: Int = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,5 +33,23 @@ class AccountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
+    override fun onResume() {
+        super.onResume()
+        buttonEdit.setOnClickListener {
+            val intent = Intent(this.activity, EditAccountActivity::class.java)
+            startActivity(intent)
+        }
 
+        image_visibilitypassword.setOnClickListener {
+            image*=-1
+
+            if(image == -1){
+                image_visibilitypassword.setImageResource(R.drawable.ic_visibility_off_black_24dp)
+                text_password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_NORMAL
+            }else if(image == 1){
+                image_visibilitypassword.setImageResource(R.drawable.ic_visibility_black_24dp)
+                text_password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+        }
+    }
 }
