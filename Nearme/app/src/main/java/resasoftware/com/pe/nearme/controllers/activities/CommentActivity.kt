80eb7d6330931f492a90bed307285bf01de5b7a1
CodigoFuente05 <42646296+CodigoFuente05@users.noreply.github.com>
2019-05-25 01:47:29 -0500

    Enterprise Details

---
+++ Nearme/app/src/main/java/resasoftware/com/pe/nearme/controllers/activities/CommentActivity.kt
@@ -0,0 +1,13 @@
+package resasoftware.com.pe.nearme.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import resasoftware.com.pe.nearme.R
+
+class CommentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
---
+++ Nearme/app/src/main/java/resasoftware/com/pe/nearme/controllers/activities/EnterpriseDetailsActivity.kt
@@ -0,0 +1,12 @@
+package resasoftware.com.pe.nearme.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterpriseDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enterprise_details)
+    }
+}
